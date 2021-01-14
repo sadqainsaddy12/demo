aec09b95c59c700f0a52368d7a91041fd4d5cf20
sadqainsaddy12 <57484645+sadqainsaddy12@users.noreply.github.com>
2021-01-14 15:06:29 +0500

    Add files via upload

--- demo.blade.php
+++ demo.blade.php
@@ -18,11 +18,49 @@
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
             Launch demo modal
           </button>
-
+<li class="dropdown-item selected" data-value="selected">selected</li>
+<li class="dropdown-item conducted" data-value="conducted">conducted</li>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal1">
+    Launch conducted
+  </button>
         <a class="dropdown-item" href="#">Link 1</a>
       <a class="dropdown-item" href="#">Link 2</a>
       <a class="dropdown-item" href="#">Link 3</a>
 
+    </div>
+  </div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel1">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+<form action="{{  url('add_image') }}" method="post"   enctype="multipart/form-data" >
+
+    {{ csrf_field() }}
+
+
+<input type="date"  name="set_date">
+
+
+<input type="submit">
+
+</form>
+
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -96,6 +134,17 @@
         imagesPreview(this, 'div.gallery');
     });
 });
+
+// $('.selected').click(function(){
+// alert($('.selected').attr('data-value'));
+
+// })
+// $('.conducted').click(function(){
+// alert($('.conducted').attr('data-value'));
+
+// })
+
+
 
 
     })
